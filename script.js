dc6e7105821bdf80f6a8b96e16815895897778b3
pik8014 <pierre.konan@mavs.uta.edu>
2025-09-13 17:35:38 -0500

    third progressive bar done

--- script.js
+++ script.js
@@ -5,3 +5,17 @@
 
 
 //* Third progress bar
+
+let items = document.querySelectorAll(".link")
+
+items.forEach((item, index) => {
+    item.addEventListener("click", ()=> {
+        for(let i = 0; i <= index; i++) {
+            items[i].classList.add("active")
+        }
+        
+        for(let j = index + 1; j < items.length; j++) {
+            items[j].classList.remove("active")
+        }
+    })
+})
